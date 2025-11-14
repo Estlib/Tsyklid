316def991161ac09d64b83b6625da969e22a0068
Estlib <75094683+Estlib@users.noreply.github.com>
2025-11-14 15:48:13 +0200

    lisasin kordamisülesanded kontrolltööks

---
+++ KordamisÜlesanded/TsükliteKordamine/Program.cs
@@ -0,0 +1,63 @@
+﻿// 1. külalised sünnipäeval
+
+/* dowhile
+küsi kasutajalt tema eesnime, tsükkel peab toimima niikaua kuni kasutaja on sisestanud midagi, et sisestus tühi "" ei oleks.
+*/
+
+using System.Linq.Expressions;
+
+Console.WriteLine("Tere kasutaja palun sisesta oma eesnimi");
+string nimi = "";
+do
+{
+    Console.WriteLine("Sinu nimi on:");
+    nimi = Console.ReadLine();
+} while (nimi == "");
+
+/* while
+siis küsi kasutajalt tema sünnikuupäeva kolme while tsükliga, kõigepealt päev, arvuna, siis kuu, nimena, ning siis aasta, arvuna
+päeva küsimise juures tuleb jälgida et kuupäev oleks vahemikus 1-31,
+kuu küsimise juures peab uuesti küsima, kui nimetus ei esine programmis (kas switch-case, if-elseif, või .Contains() abiga)
+aasta juures ei tohi olla sünniaasta 19ndas sajandis (18xx) ega tulevikus. (tekib ka vahemik)
+*/
+
+Console.WriteLine("Sisesta oma sünnikuupäeva päev");
+int sünnikuupäev = 0;
+while (sünnikuupäev > 31 || sünnikuupäev < 1)
+{
+    Console.WriteLine("Palun sisesta õige kuupäev vahemikus 1-31");
+    sünnikuupäev = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Sisesta ka oma sünnikuu");
+string sünnikuu = "";
+while (sünnikuu == "")
+{
+    Console.WriteLine("sisesta õige kuu:");
+    sünnikuu = Console.ReadLine();
+    switch (sünnikuu)
+    {
+        case "detsember":
+            Console.WriteLine("Oled sisestanud 12.nda kuu - detsembri");
+            break;
+        default:
+            Console.WriteLine("Ei tunne sellist sünnikuud");
+            sünnikuu = "";
+            break;
+    }
+}
+
+/* for
+küsi kasutajalt tema sünnipäevale küllatulijaid uude listi for tsükliga, iga tsükkel sisestab uue inimesenime, kuni kasutaja ütleb "kõik"
+*/
+
+/* foreach
+küsi kasutajalt kas on mõni külaline keda ta siiski näha ei tahaks? ning võrdle foreach tsüklis kõik külalised kasutaja sisestatuga.
+eemalda see külaline kui ta nimekirjas on, kui külalist nimekirjast ei leita, siis ütle "sellist inimest sa pole kutsunudki"
+*/
+
+
+//==========================================================================
+/* 2. Üldine ülesanne
+kirjuta programm mis küsib kasutajalt tema lemmikvärvi, ning joonistab talle tema lemmimkvärvi ruudu, mille sisse kirjutad vastava värvi ainult keskmisele reale. Kasutada võib vabalt valitud tsüklitüüpi
+*/
